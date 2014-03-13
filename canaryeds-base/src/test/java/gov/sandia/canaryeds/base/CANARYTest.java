fe14172e60c48cac98654540e09a4f7731cb77d4
dbhart <dbhart@2f590c40-3df0-0310-96b9-c5ab34f8a05d>
2014-03-13 17:46:15 +0000

    Stupid windows
    git-svn-id: https://software.sandia.gov/svn/teva/canary/canaryeds/trunk@4280 2f590c40-3df0-0310-96b9-c5ab34f8a05d

---
+++ canaryeds-base/src/test/java/gov/sandia/canaryeds/base/CANARYTest.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2014 Sandia Corporation.
+ * Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
+ * Government retains certain rights in this software.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This software was written as part of an Inter-Agency Agreement between Sandia
+ * National Laboratories and the US EPA NHSRC.
+ */
+package gov.sandia.canaryeds.base;
+
+import gov.sandia.seme.framework.Controller;
+import gov.sandia.seme.framework.ModelConnection;
+import gov.sandia.seme.framework.ConfigurationException;
+import gov.sandia.seme.framework.InitializationException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CANARYTest {
+
+    static CopyOnWriteArrayList<ModelConnection> stationsV5mvnn;
+    static CopyOnWriteArrayList<ModelConnection> stationsV5lpcf;
+    static URL v5mvnn = CANARYTest.class.getResource(
+            "/gov/sandia/canaryeds/v5config.yml");
+    static URL v5lpcf = CANARYTest.class.getResource(
+            "/gov/sandia/canaryeds/v5lpcf.yml");
+    static URL stationCSV = CANARYTest.class.getResource(
+            "/gov/sandia/canaryeds/Tutorial_Station_B.csv");
+
+    public CANARYTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of CANARY#configure method.
+     */
+    @Test
+    public void testConfigure() {
+        try {
+            CANARY eds = new CANARY();
+            InputStream is = v5mvnn.openStream();
+            HashMap config = eds.parseYAMLStream(is);
+            HashMap stationB_In = (HashMap) ((Map) ((Map) config.get(
+                    "connections")).get("stationb_in")).get("text.CSVReaderWide");
+            stationB_In.put("location", stationCSV);
+            eds.configure(config);
+        } catch (ConfigurationException | IOException ex) {
+            fail("The test caused an exception: " + ex.getMessage());
+        }
+    }
+
+    /**
+     * Tests the CANARY class and the workflows.MVNN_BED class.
+     */
+    @Test
+    public void testShortMVNN() {
+        try {
+            CANARY eds = new CANARY();
+            InputStream is = v5mvnn.openStream();
+            HashMap config = eds.parseYAMLStream(is);
+            HashMap stationB_In = (HashMap) ((Map) ((Map) config.get(
+                    "connections")).get("stationb_in")).get("text.CSVReaderWide");
+            stationB_In.put("location", stationCSV);
+            eds.configure(config);
+            eds.initialize();
+            Controller ctrl = eds.getController();
+            ctrl.run();
+            eds.shutdown();
+        } catch (InitializationException | ConfigurationException | IOException ex) {
+            fail("The test caused an exception: " + ex.getMessage());
+        }
+    }
+
+    /**
+     * Tests the CANARY class and the workflows.LPCF_BED class.
+     */
+    @Test
+    public void testShortLPCF() {
+        try {
+            CANARY eds = new CANARY();
+            InputStream is = v5lpcf.openStream();
+            HashMap config = eds.parseYAMLStream(is);
+            HashMap stationB_In = (HashMap) ((Map) ((Map) config.get(
+                    "connections")).get("stationb_in")).get("text.CSVReaderWide");
+            stationB_In.put("location", stationCSV);
+            eds.configure(config);
+            eds.initialize();
+            Controller ctrl = eds.getController();
+            ctrl.run();
+            eds.shutdown();
+        } catch (InitializationException | ConfigurationException | IOException ex) {
+            fail("The test caused an exception: " + ex.getMessage());
+        }
+    }
+
+}
---
+++ canaryeds-external-eddies/src/main/java/gov/sandia/canaryeds/external/eddies/EDDIESController.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2014 Sandia Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gov.sandia.canaryeds.external.eddies;
+
+import gov.sandia.seme.framework.Descriptor;
+import gov.sandia.seme.util.ControllerImpl;
+
+/**
+ * @author dbhart
+ */
+public class EDDIESController extends ControllerImpl {
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public Descriptor getConfiguration() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void loadState() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void saveState() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
